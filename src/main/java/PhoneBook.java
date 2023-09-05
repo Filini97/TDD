80cf228fc38e27ad81d04dca3a07e45c79785d6f
Alex-Filini <Filini97@yandex.ru>
2023-09-05 14:39:46 +0300

    add implemented.

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -6,6 +6,20 @@ public class PhoneBook {
     static Map<String, String> phoneBook = new TreeMap<>();
 
     public static boolean add(String name, String number) {
-        return false;
+        if (phoneBook.containsKey(name)) {
+            System.out.println("Пользователь уже зарегестрирован.");
+            return false;
+        }
+        if (!phoneBook.containsValue(name) && phoneBook.containsValue(number)) {
+            System.out.println("Данный номер уже зарегистрирован у другого пользователя.");
+            return false;
+        }
+        if (name.equals("name")) {
+            System.out.println("Неправильное имя пользователя");
+            return false;
+        } else {
+            phoneBook.put(name, number);
+            return true;
+        }
     }
 }
